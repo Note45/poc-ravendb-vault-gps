7b9bf436fa258f6cfa2505be01ca2cbdf6e5bde3
Note45 <antonion.2048@gmail.com>
2025-10-21 08:46:20 -0300

    tests: add unit tests GpsPositionRepositoryTests

---
+++ Vault.Gps.Tests/Infra/Database/Repositories/GpsPositionRepositoryTests.cs
@@ -0,0 +1,91 @@
+using System.Linq;
+using Raven.TestDriver;
+using Raven.Client.Documents;
+using NSubstitute;
+using Raven.Embedded;
+using vault_gps.Contracts.Models;
+using vault_gps.Infra.Database.Contracts;
+using vault_gps.Infra.Database.Repositories;
+using Xunit;
+
+namespace Vault.Gps.Tests.Infra.Database.Repositories;
+
+public class GpsPositionRepositoryTests: RavenTestDriver
+{
+    static GpsPositionRepositoryTests()
+    {
+        ConfigureServer(new TestServerOptions
+        {
+            CommandLineArgs = new List<string>
+            {
+                "--License.Eula.Accepted=true"
+            },
+            Licensing = new ServerOptions.LicensingOptions
+            {
+                ThrowOnInvalidOrMissingLicense = false
+            }
+        });
+    }
+    
+    [Fact(DisplayName = "Should be able to save the gps position item")]
+    public async Task Should_Be_Able_To_Save_Position()
+    {
+        //Act
+        var dockStoreMock = Substitute.For<IDocumentStoreHolder>();
+        var repo = new GpsPositionRepository(dockStoreMock);
+        var positionItem = new GpsPositionItem()
+        {
+            EventType = "EventType",
+            Latitude = "Latitude",
+            Longitude = "Longitude",
+            UpdateTime = DateTime.Now.ToShortDateString(),
+            Type = "Type"
+        };
+        
+        //Arrange
+        var result = await repo.saveGpsPositionItem(positionItem);
+        
+        //Assert
+        Xunit.Assert.Equivalent(result, positionItem);
+    }
+    
+    [Fact(DisplayName = "Should be able to get all the gps position items")]
+    public async Task Should_Be_Able_To_Get_All_Gps_Position_Items()
+    {
+        using (var store = GetDocumentStore())
+        {
+            // Arrange
+            using (var session = store.OpenAsyncSession())
+            {
+                await session.StoreAsync(new GpsPositionItem {             
+                    EventType = "EventType 1",
+                    Latitude = "Latitude 1",
+                    Longitude = "Longitude 1",
+                    UpdateTime = DateTime.Now.ToShortDateString(),
+                    Type = "Type 1" 
+                });
+                await session.StoreAsync(new GpsPositionItem {             
+                    EventType = "EventType 2",
+                    Latitude = "Latitude 2",
+                    Longitude = "Longitude 2",
+                    UpdateTime = DateTime.Now.ToShortDateString(),
+                    Type = "Type 2" 
+                });
+                await session.SaveChangesAsync();
+            }
+
+            WaitForIndexing(store); 
+
+            var documentHolderMock = Substitute.For<IDocumentStoreHolder>();
+            documentHolderMock.CreateStore().Returns(store);
+
+            var repository = new GpsPositionRepository(documentHolderMock);
+
+            // Act
+            var result = await repository.getAllGpsPositionItems(0, 10);
+
+            // Assert
+            Xunit.Assert.Equivalent(2,  result.Count());
+        }
+    }
+}
