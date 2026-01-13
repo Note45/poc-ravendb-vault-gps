e079b5211d3f8300810388496c8cd7b43f089425
Note45 <antonion.2048@gmail.com>
2026-01-13 08:44:13 -0300

    chore: change GpsPositionController constructor to primary

--- Vault.Gps/Controllers/GpsPositionController.cs
+++ Vault.Gps/Controllers/GpsPositionController.cs
@@ -7,19 +7,12 @@ namespace vault_gps.Controllers;
 
 [ApiController]
 [Route("api/gps")]
-public class GpsPositionController: ControllerBase
+public class GpsPositionController(IGpsPositionService service) : ControllerBase
 {
-    private IGpsPositionService _service;
-
-    public GpsPositionController(IGpsPositionService service)
-    {
-        _service = service;
-    }
-    
     [HttpPost]
     public async Task<IActionResult> PostPosition([FromBody] CreateGpsPositionCommand command)
     {
-        var position = await _service.saveGpsPosition((GpsPositionItem)command);
+        var position = await service.saveGpsPosition((GpsPositionItem)command);
         
         return Ok(position);
     }  
@@ -27,7 +20,7 @@ public class GpsPositionController: ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetPositions([FromQuery] int page = 0, [FromQuery] int size = 30)
     {
-        var results = await _service.getAllGpsPosition(page, size);
+        var results = await service.getAllGpsPosition(page, size);
         
         return Ok(results);
     }    
