f3e798ade7d30eb3b5675c1a8ff9c17718b749e4
Note45 <antonion.2048@gmail.com>
2026-01-14 22:35:37 -0300

    tests: add new tests to Latitude and Longitude validations

--- Vault.Gps.Tests/Domain/Validators/CreateGpsPositionCommandValidatorTests.cs
+++ Vault.Gps.Tests/Domain/Validators/CreateGpsPositionCommandValidatorTests.cs
@@ -51,6 +51,31 @@ public class CreateGpsPositionCommandValidatorTests
 
         //Assert    
         Assert.False(result.IsValid);
-        Assert.Equivalent(7, result.Errors.Count);
+        Assert.Equivalent(9, result.Errors.Count);
+    }
+    
+    [Fact(DisplayName = "Should be able to show validate command errors")]
+    public void Should_Be_Able_To_Show_Validate_Latitute_And_Longitude_Errors()
+    {
+        //Act
+        var validate = new CreateGpsPositionCommandValidator();
+        var command = new CreateGpsPositionCommand()
+        {
+            AggregateId = "12312",
+            EventType = nameof(EventTypeEnum.GpsPositionItemCreated),
+            Latitude = "fadfadfa",
+            Longitude = "fadfadf",
+            UpdateTime = DateTime.UtcNow.ToString(),
+            Description = "Home",
+        }; 
+        
+        //Arrange 
+        var result = validate.Validate(command);
+
+        //Assert    
+        Assert.False(result.IsValid);
+        Assert.Equivalent(2, result.Errors.Count);
+        Assert.Equivalent("'Latitude' must only contain numbers", result.Errors.First().ErrorMessage);
+        Assert.Equivalent("'Longitude' must only contain numbers", result.Errors.Last().ErrorMessage);
     }
 }
